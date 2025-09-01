0ba7a613e3055b010d403df4d6905b80e89dc95d
csprats <222044220+csprats@users.noreply.github.com>
2025-09-01 10:27:10 +0200

    Initial push

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+export const App = () => {
+  return (
+    <>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item">
+          CSValley
+        </a>
+      </div>
+
+    <div id="navbarBasicExample" className="navbar-menu">
+      <div className="navbar-start">
+        <a className="navbar-item" href="https://csvalley.dev">
+          Home page
+        </a>
+
+        <a className="navbar-item">
+          Code
+        </a>
+      </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <h5 className="subtitle is-5">by csprats</h5>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <img className="rounded" src="https://csvalley.dev/assets/csvalley_logo-BrqXZeOU.jpg" alt="csvalley" width="200"></img>
+      <p>Hi, I'm csprats and this is my independent study, 
+		I do Progamate video games and create web pages.</p>
+		<p>I've created a few games, like: </p>
+		<ul>
+			<li>
+				<a href="https://csprats.itch.io/space-galaxy-war" target="_blank">Space Galaxy War</a>
+			</li>
+			 <li>
+				 <a href="https://csprats.itch.io/boat-war" target="_blank">Boat War</a>
+			 </li>
+		 </ul>
+		 <p>These two are published to itchio</p>
+		 <p>And I have a famous game created in vanilla javascript:</p>
+		 <p><a href="https://asteroids.csvalley.dev/">JS-Asteroids</a></p>
+		 <p>And more!: </p>
+		 <ul>
+			 <li><a href="https://mole.csvalley.dev/">Whac a mole game,</a></li>
+			 <li><a href="https://js-pong-game-2.netlify.app/">Two player pong game,</a></li>
+			 <li><a href="https://stack-tower-game.netlify.app/">Stack tower game,</a></li>
+			 <li><a href="https://js-akanoid.netlify.app/">Arkanoid game,</a></li>
+			 <li><a href="https://js-jumper.netlify.app/">Jumper game</a></li>
+			 <li><a href="https://js-tetris-csvalley.netlify.app/">Tetris game</a></li>
+		 </ul>
+		 <p>I created a few tools:</p>
+		 <ul>
+			<li><a href="https://csvalley.dev/writter/">writter,</a></li>
+			<li><a href="https://csvalley.dev/text-editor/">CSText</a></li>
+			<li><a href="https://csvblog.netlify.app">CSBlog</a></li>
+		 </ul>
+
+		 <p>And a page of characters!</p>
+		 <ul>
+			<li><a href="https://chracters-dragon-ball.netlify.app/">Dragon Ball Page</a></li>
+			<li><a href="https://zelda-characters.netlify.app/">Zelda BOTH Page</a></li>
+		 </ul>
+    </>
+  )
+}
